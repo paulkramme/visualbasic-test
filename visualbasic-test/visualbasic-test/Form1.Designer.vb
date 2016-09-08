d89c04ccab6b7bba02e62dd229f2658e48c54814
Paul Kramme <pjkramme@gmail.com>
2016-09-08 20:39:48 +0200

    Change picture

--- visualbasic-test/visualbasic-test/Form1.Designer.vb
+++ visualbasic-test/visualbasic-test/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
@@ -40,8 +41,9 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(382, 172)
         Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Click dat Button m8!"
         Me.ResumeLayout(False)
 
     End Sub
